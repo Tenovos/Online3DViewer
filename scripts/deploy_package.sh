f6f1cd195a61148ed05661205bffecebdde67d95
tenovos-sebastiano <sebastiano.gargiulo@tenovos.com>
2023-03-16 01:57:50 -0400

    chore(scripts): create bucket, distribution and policy if needed

--- scripts/deploy_package.sh
+++ scripts/deploy_package.sh
@@ -29,7 +29,7 @@ then
     echo "S3 Bucket already exists. Continuing..."
 else
     echo "S3 Bucket does not exist. Creating required bucket..."
-    #aws s3api create-bucket --bucket tenovos-3d-object-viewer-$AWS_PROFILE --region us-east-1
+    aws s3api create-bucket --bucket tenovos-3d-object-viewer-$AWS_PROFILE --region us-east-1
 fi
 
 # Check if CloudFront Distribution exists for bucket
@@ -40,11 +40,16 @@ then
     echo "Distribution [$DISTRIBUTION_ID] already exists for bucket."
 else
     echo "Distribution does not exist for bucket. Creating new distribution..."
-    #aws cloudfront create-origin-access-control --origin-access-control-config Name=tenovos-3d-object-viewer-$AWS_PROFILE.s3.us-east-1.amazonaws.,SigningProtocol=sigv4,SigningBehavior=always,OriginAccessControlOriginType=s3
-    #aws cloudfront create-distribution --origin-domain-name tenovos-3d-object-viewer-$AWS_PROFILE.s3.amazonaws.com --default-root-object index_min.html
-    #aws s3api put-bucket-policy --bucket tenovos-3d-object-viewer-$AWS_PROFILE --policy file://s3-policy.json
+    aws cloudfront create-distribution --origin-domain-name tenovos-3d-object-viewer-$AWS_PROFILE.s3.amazonaws.com
+    aws cloudfront create-origin-access-control --origin-access-control-config Name=tenovos-3d-object-viewer-$AWS_PROFILE.s3.us-east-1.amazonaws.,SigningProtocol=sigv4,SigningBehavior=always,OriginAccessControlOriginType=s3
 
     DISTRIBUTION_ID=$(aws cloudfront list-distributions --query "DistributionList.Items[*].{id:Id,origin:Origins.Items[0].Id}[?contains(origin, 'tenovos-3d-object-viewer')].id" --output text)
+
+    DISTRIBUTION_ARN=$(aws cloudfront get-distribution --id $DISTRIBUTION_ID --query 'Distribution.ARN' --output text)
+    s3PolicyJson=s3-policy.json
+    echo "$( jq '.Statement[].Resource = "arn:aws:s3:::tenovos-3d-object-viewer-'$AWS_PROFILE'/*" | .Statement[].Condition.StringEquals."AWS:SourceArn" = "'$DISTRIBUTION_ARN'"' $s3PolicyJson)" > $s3PolicyJson
+
+    aws s3api put-bucket-policy --bucket tenovos-3d-object-viewer-$AWS_PROFILE --policy file://$s3PolicyJson
 fi
 
 # Upload package to S3 Bucket
